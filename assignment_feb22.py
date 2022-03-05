5a1619f2c90d0e02f9617b8f28861b582f2a0da5
christabel <christabelawah@yahoo.com>
2022-03-05 03:11:11 +0000

    basics python1

---
+++ assignment_feb22.py
@@ -0,0 +1,47 @@
+#what is the maximum length of a python idntifier?
+
+from xml.dom.pulldom import CHARACTERS
+
+
+Answer = 79 CHARACTERS
+
+
+#which of the following is an invalid statement
+num = 1000000
+x y z = 1 2 3
+x,y,z = 1, 2, 3
+x_y_z = 10000000
+
+Answer = x y z = 1 2 3
+
+#Which of the following are valid Python variable names:
+ver1.3
+return
+home_address
+route66
+Age
+4square
+
+Answer = ver1.3, home_address, route66, Age
+
+#All keywords in Python are in _________ *
+Lower case
+Upper case
+None of the mentioned
+Capitalized
+
+Answer = None of the mentioned
+
+
+#Which of the following are Python reserved words (keywords):
+default
+and
+goto
+class
+None
+
+Answer = and, class,none
+
+
+
+
