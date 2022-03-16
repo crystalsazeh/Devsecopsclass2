34ca2a7ff3f7d0349607d0b303c83d134684b245
christabel <christabelawah@yahoo.com>
2022-03-16 06:07:39 +0000

    assissgment 3_builtin

---
+++ 3_builtin.py
@@ -0,0 +1,12 @@
+A Tuple
+A tuple in Python is similar to a list. The difference between the two is that we cannot change the elements of a tuple once it is assigned whereas we can change the elements of a list.
+
+A Type
+type()	Returns the type of an object
+type() method returns class type of the argument(object) passed as parameter. type() function is mostly used for debugging purposes.
+
+Two different types of arguments can be passed to type() function, single and three argument. If single argument type(obj) is passed, it returns the type of given object. If three arguments type(name, bases, dict) is passed, it returns a new type object.
+
+
+Vars 
+vars()	Returns the __dict__ property of an object
